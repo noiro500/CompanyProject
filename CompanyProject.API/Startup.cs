5f01c3003f4e4fe459294427f266aa495d116d5e
noiro <noiro@NOIRO-PC>
2020-09-30 21:34:37 +0300

    Добавлена DI

--- CompanyProject.API/Startup.cs
+++ CompanyProject.API/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,26 +16,40 @@ namespace CompanyProject.API
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            var configConnectionToDb = Configuration["DataBase:PsqlRepository:ConnectionString"];
+            services.AddRepository(configConnectionToDb);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStatusCodePages();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
---
+++ CompanyProject.Repository/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CompanyProject.Domain.CompanyContactAggregate;
+using CompanyProject.Domain.CustomerAggregate;
+using CompanyProject.Domain.EmployeeAggregate;
+using CompanyProject.Domain.FeedbackAggregate;
+using CompanyProject.Domain.MessageAggregate;
+using CompanyProject.Domain.OrderAggregate;
+using CompanyProject.Domain.PageAggregate;
+using CompanyProject.Domain.ParagraphAggregate;
+using CompanyProject.Domain.PriceListAggregate;
+using CompanyProject.Repository.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CompanyProject.Repository
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services, string configConnectionToDb)
+        {
+            services.AddTransient<ICompanyContactsRepository, CompanyContactsRepository>();
+            services.AddTransient<ICustomersRepository, CustomersRepository>();
+            services.AddTransient<IEmployeesRepository, EmployeesRepository>();
+            services.AddTransient<IFeedbacksRepository, FeedbacksRepository>();
+            services.AddTransient<IMessagesRepository, MessagesRepository>();
+            services.AddTransient<IOrdersRepository, OrdersRepository>();
+            services.AddTransient<IPagesRepository, PagesRepository>();
+            services.AddTransient<IParagraphsRepository, ParagraphsRepository>();
+            services.AddTransient<IPriceListsRepository, PriceListsRepository>();
+            services.AddDbContext<CompanyProjectDbContext>(options =>
+                options.UseNpgsql(configConnectionToDb));
+            return services;
+        }
+    }
+}
