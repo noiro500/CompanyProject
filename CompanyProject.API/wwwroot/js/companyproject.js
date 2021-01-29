be5ec08cf8b02949c0aaa62fcc6409f2f30964d2
noiro <noiro@NOIRO-PC>
2021-01-29 04:22:04 +0300

    Мелкие правки

--- CompanyProject.API/wwwroot/js/companyproject.js
+++ CompanyProject.API/wwwroot/js/companyproject.js
@@ -67,25 +67,6 @@
         $(this).toggleClass("is-active");
     });
 
-    $.datepicker.regional['ru'] = {
-        closeText: 'Закрыть',
-        prevText: 'Предыдущий',
-        nextText: 'Следующий',
-        currentText: 'Сегодня',
-        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-        dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-        dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-        dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        weekHeader: 'Не',
-        dateFormat: 'dd.mm.yy',
-        firstDay: 1,
-        isRTL: false,
-        showMonthAfterYear: false,
-        yearSuffix: ''
-    };
-    $.datepicker.setDefaults($.datepicker.regional['ru']);
-
     //Всплывающее окно Toast на политику конфиденциальности
     //$("input[type='checkbox']#privacy-policy").on('change',function() {
     //    if (!$("input[type='checkbox']#privacy-policy").prop('checked')) {
