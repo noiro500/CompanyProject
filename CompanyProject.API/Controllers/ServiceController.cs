badb1b89fd7d986b00c02447e189efe6c8ff0b68
noiro <noiro@NOIRO-PC>
2021-03-17 15:58:34 +0300

    Правки

--- CompanyProject.API/Controllers/ServiceController.cs
+++ CompanyProject.API/Controllers/ServiceController.cs
@@ -38,7 +38,7 @@ namespace CompanyProject.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> MakeOrder(OrderViewModel order)
+        public IActionResult MakeOrder(OrderViewModel order)
         {
             return View();
         }
@@ -62,6 +62,7 @@ namespace CompanyProject.API.Controllers
             if (order != null)
             {
                 var orderProperties = order.GetType().GetProperties();
+                ViewBag.OrderPropCount = orderProperties.Length;
                 foreach (var propertyInfo in orderProperties)
                 {
                     if (propertyInfo.GetValue(order) == null)
