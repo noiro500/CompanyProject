9910b5ca4dbd5082aef5fc187646d2058b2143bf
noiro <noiro@NOIRO-PC>
2021-05-25 12:06:20 +0300

    Настройка AutoMapper

---
+++ CompanyProject.API/Infrastructure/Log/Log.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CompanyProject.API.Infrastructure.Log
+{
+    internal static class Log
+    {
+        internal static ILoggerFactory LoggerFactory { get; set; }
+        internal static ILogger CreateLogger<t>() => LoggerFactory.CreateLogger<t>();
+        internal static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName);
+    }
+}
