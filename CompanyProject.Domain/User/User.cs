7c8bfba273af3b692456af187b4da080da3cdee6
noiro <noiro@NOIRO-PC>
2022-03-11 05:17:32 +0300

    Добавлен универсальный класс User. Начало добавления ASP.NET Identity

---
+++ CompanyProject.Domain/User/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CompanyProject.Domain.User
+{
+    internal class User : IdentityUser
+    {
+        public string Position { get; set; }
+        public string WhatsAppPhoneNumber { get; set; } = "Отсутствует";
+        public bool IsAdoptedPrivacyPolicy { get; set; }
+        public List<Order.Order> Orders { get; set; } = new List<Order.Order>();
+
+        public string PassportNumber { get; set; }
+        //[DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+}
