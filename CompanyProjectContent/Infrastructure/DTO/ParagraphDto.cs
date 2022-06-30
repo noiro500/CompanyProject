8598208672bd7356bd4ac25ff0896d4424980601
noiro <noiro@NOIRO-PC>
2022-06-30 03:28:51 +0300

    CompanyProjectContent отвечает на запрос

---
+++ CompanyProjectContent/Infrastructure/DTO/ParagraphDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CompanyProjectContent.Infrastructure.DTO;
+
+public class ParagraphDto
+{
+    public int ParagraphId { get; set; }
+    public bool IsGlobalTitle { get; set; } = false;
+    public bool IsSubtitle { get; set; } = false;
+    public bool HasPicture { get; set; } = false;
+    public string? PicturePath { get; set; } = null;
+    public bool IsList { get; set; } = false;
+    public bool IsMobileVisible { get; set; } = false;
+    public string[] Content { get; set; }
+}
--- CompanyProjectContent/Program.cs
+++ CompanyProjectContent/Program.cs
@@ -1,18 +1,25 @@
 using System.Configuration;
+using System.Text.Json.Serialization;
 using CompanyProjectContent.Infrastructure;
 using CompanyProjectContent.Infrastructure.InitialDBContent;
+using EntityFrameworkCore.UnitOfWork.Extensions;
 using Microsoft.EntityFrameworkCore;;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    /*.AddJsonOptions(options=>
+        options.JsonSerializerOptions.ReferenceHandler=ReferenceHandler.IgnoreCycles)*/;
 builder.Services.AddDbContext<CompanyProjectContentDbContext>(options =>
     options.UseNpgsql(builder.Configuration["ConnectionStrings:ConnectionStringToPostgreSQLAzure"], sqlOptions =>
             sqlOptions.MigrationsAssembly(typeof(CompanyProjectContentDbContext).Assembly.GetName().Name)));
 
-//builder.Services.AddScoped<DbContext, CompanyProjectContentDbContext>();
+
 builder.Services.AddScoped<IInitialDbContent, InitialDbContent>();
+builder.Services.AddScoped<DbContext, CompanyProjectContentDbContext>();
+builder.Services.AddUnitOfWork();
+builder.Services.AddUnitOfWork<CompanyProjectContentDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
