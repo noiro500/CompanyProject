31ab5b25030b5cd866c1a58f24072b27d4654f54
noiro <noiro@NOIRO-PC>
2022-07-28 03:58:39 +0300

    Создана модель Card и начато создание контекста бд CompanyProjectCardDbContext

---
+++ CompanyProjectCardsService/Infrastructure/InitialDbContent/InitialDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CompanyProjectCardsService.Model;
+
+namespace CompanyProjectCardsService.Infrastructure.InitialDbContent;
+
+public class InitialDbContext : IInitialDbContext
+{
+    public IList<Card> InitialDbCardsContent()
+    {
+        throw new NotImplementedException();
+    }
+}
