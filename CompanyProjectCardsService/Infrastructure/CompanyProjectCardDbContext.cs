5e084c69527f4483cdd8074a5be833605370ab5e
noiro <noiro@NOIRO-PC>
2022-08-10 03:03:05 +0300

    Создана модель Card

--- CompanyProjectCardsService/Infrastructure/CompanyProjectCardDbContext.cs
+++ CompanyProjectCardsService/Infrastructure/CompanyProjectCardDbContext.cs
@@ -4,20 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CompanyProjectCardsService.Infrastructure;
 
-public class CompanyProjectCardDbContext:DbContext
+public class CompanyProjectCardDbContext : DbContext
 {
     private readonly IInitialDbContent _content;
     public CompanyProjectCardDbContext(DbContextOptions<CompanyProjectCardDbContext> options,
-        IInitialDbContent ctn):base(options)
+        IInitialDbContent ctn) : base(options)
     {
-        _content=ctn;
+        _content = ctn;
     }
     public DbSet<Card> Cards { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Card>().HasData(
-            _content.InitialDbCardsContent()
-        );
-        modelBuilder.Entity<Card>().OwnsOne(u => u.CardFooter);
+        //modelBuilder.Entity<Card>().HasData(
+        //    _content.InitialDbCardsContent()
+        //);
+        modelBuilder.Entity<CardFooter>()
+            .HasMany(p => p.CardFooterItems).WithOne();
+        modelBuilder.Entity<Card>()
+            .HasOne(c => c.CardFooter)
+            .WithOne(c => c.Card)
+            .HasForeignKey<CardFooter>(k => k.CardFooterForeignKey);
+        modelBuilder.Entity<Card>()
+            .HasOne(c => c.CardHeader)
+            .WithOne(c => c.Card)
+            .HasForeignKey<CardHeader>(k => k.CardHeaderForeignKey);
     }
 }
--- CompanyProjectCardsService/Model/Card.cs
+++ CompanyProjectCardsService/Model/Card.cs
@@ -1,22 +1,51 @@
-﻿namespace CompanyProjectCardsService.Model;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyProjectCardsService.Model;
+
 
 public class Card
 {
     public Guid CardId { get; set; }
-    public string PageNameForCard { get; set; }
-    public List<Cardheader> CardHeader { get; set; }
+    public string? PageNameForCard { get; set; }
+    public bool CardIsLink { get; set; }
+    public string? CardLinkAddress { get; set; }
+    public bool CardHasHeader { get; set; }
+    public CardHeader? CardHeader { get; set; }
     public string? CardImage { get; set; }
     public string? CardContent { get; set; }
-    public Cardfooter? CardFooter { get; set; }
+    public bool CardHasFooter { get; set; }
+    public CardFooter? CardFooter { get; set; }
+
 }
 
-public class Cardfooter
+public class CardFooter
 {
-    public List<string> CardFooterItems { get; set; }
+    public Guid CardFooterId { get; set; }
+    public List<CardFooterItem> CardFooterItems { get; set; }
+    //public Guid CardId { get; set; }
+    public Guid CardFooterForeignKey { get; set; }
+    public Card Card { get; set; }
+    
 }
 
-public class Cardheader
+public class CardFooterItem
 {
-    public string CardHeaderTitle { get; set; }
+    public Guid CardFooterItemId { get; set; }
+    public bool CardFooterItemIsLink { get; set; }
+    public string? CardFooterItemLinkAddress { get; set; }
+    public string? CardFooterItemContent { get; set; }
+
+}
+
+public class CardHeader
+{
+    public Guid CardHeaderId { get; set; }
+    public bool CardHeaderIsLink { get; set; }
+    public string? CardHeaderLinkAddress { get; set; }
+    public string? CardHeaderTitle { get; set; }
     public string? CardHeaderIcon { get; set; }
+
+    public Guid CardHeaderForeignKey { get; set; }
+    public Card Card { get; set; }
 }
+
