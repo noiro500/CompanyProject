73c33a224c2bcaa269e9ccba3c47dc4608aab0e3
noiro <noiro@NOIRO-PC>
2022-08-19 05:28:58 +0300

    CardViewModel помечен как неиспользуемый

--- CompanyProject.ViewModels/CardViewModel_IsNotUsed.cs
+++ CompanyProject.ViewModels/CardViewModel_IsNotUsed.cs
@@ -1,13 +1,13 @@
 ﻿namespace CompanyProject.ViewModels
 {
-    public class CardViewModel
+    public class CardViewModel_IsNotUsed
     {
         public string ScreenName { get; private set; }
         public string AspController { get; private set; }
         public string AspAction { get; private set; }
         public string Image { get; private set; }
 
-        public CardViewModel(string screenName, string aspController, string aspAction, string image)
+        public CardViewModel_IsNotUsed(string screenName, string aspController, string aspAction, string image)
         {
 
             ScreenName = screenName;
