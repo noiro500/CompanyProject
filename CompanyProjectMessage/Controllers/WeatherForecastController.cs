afa7e8158b0a072ddae77ea5b34405d39d99d39e
noiro500 <yai198x@gmail.com>
2022-11-08 13:06:36 +0300

    Начато добавление проекта web-api CompanyProjectMessage

---
+++ CompanyProjectMessage/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyProjectMessage.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        
+    }
+}
