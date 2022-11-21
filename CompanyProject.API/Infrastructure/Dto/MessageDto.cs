506d67c9fd2e983f2059e5f87d6e6349f5c210b1
noiro500 <yai198x@gmail.com>
2022-11-22 02:00:01 +0300

    обновление javascript библиотек

--- CompanyProject.API/Infrastructure/Dto/MessageDto.cs
+++ CompanyProject.API/Infrastructure/Dto/MessageDto.cs
@@ -32,20 +32,20 @@ namespace CompanyProject.API.Infrastructure.Dto
     {
         public MessageDtoValidator()
         {
-            RuleFor(x => x.PersonName).NotEmpty().WithMessage("Не указано имя").Matches(@"^[а-яА-Я0-9._%+""'\s-]*$")
-                .WithMessage("Некорректные символы. Допускаются только русские символы!")
+            RuleFor(x => x.PersonName)
+                .NotNull().WithMessage("Не указано имя")
+                .Matches(@"^[а-яА-Я0-9._%+""'\s-]*$").WithMessage("Некорректные символы. Допускаются только русские символы!")
                 .MaximumLength(30).WithMessage("Максимальная длина имени - 30 символов");
-            ;
             RuleFor(x => x.WhatsAppTelegramNumber).NotEmpty().WithMessage("Не указан номер WhatsApp/Telegram");
-            RuleFor(x => x.Email).EmailAddress()
-                .WithMessage("Некорректный адрес электронной почты");
-            RuleFor(x => x.SubjectMessage).NotEmpty().WithMessage("Не указана тема сообщения")
-                .Matches(@"^[а-яА-Я0-9._%+""'\s-]*$")
-                .WithMessage("Некорректные символы. Допускаются только русские символы!")
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("Некорректный адрес электронной почты");
+            RuleFor(x => x.SubjectMessage)
+                .NotEmpty().WithMessage("Не указана тема сообщения")
+                .Matches(@"^[а-яА-Я0-9._%+""'\s-]*$").WithMessage("Некорректные символы. Допускаются только русские символы!")
                 .MaximumLength(50).WithMessage("Максимальная длина темы - 50 символов");
-            RuleFor(x => x.Content).NotEmpty().WithMessage("Не введено сообщение")
-                .Matches(@"^[а-яА-Я0-9._%+""'\s-]*$")
-                .WithMessage("Некорректные символы. Допускаются только русские символы!")
+            RuleFor(x => x.Content)
+                .NotEmpty().WithMessage("Не введено сообщение")
+                .Matches(@"^[а-яА-Я0-9._%+""'\s-]*$").WithMessage("Некорректные символы. Допускаются только русские символы!")
                 .Length(20, 2000).WithMessage("Длина сообщения должна быть от 20 до 2000 символов");
         }
     }
