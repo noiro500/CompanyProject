6569ca79f11845cc3d1620ed7e70d88dc3247f64
noiro500 <yai198x@gmail.com>
2022-11-26 08:19:48 +0300

    Мелкие правки

--- CompanyProject.API/Infrastructure/RefitInterfaces/IContentServiceMessage.cs
+++ CompanyProject.API/Infrastructure/RefitInterfaces/IContentServiceMessage.cs
@@ -7,8 +7,8 @@ namespace CompanyProject.API.Infrastructure.RefitInterfaces
 {
     public interface IContentServiceMessage
     {
-        //[Post("/gateway/v1/Message")]
-        [Post("/api/v1/Message/")]
+        [Post("/gateway/v1/Message")]
+        //[Post("/api/v1/Message/")]
         Task<ApiResponse<MessageDto>> Post([Body] Dictionary<string, string> data);
     }
 }
