4ab60bf370114584b409a1b3538032da40c542b7
noiro500 <yai198x@gmail.com>
2022-12-08 16:52:49 +0300

    Начато добавление микросервиса CompanyProjectAddressService

--- CompanyProjectContent/Infrastructure/InitialDbContent/InitialDbContent.cs
+++ CompanyProjectContent/Infrastructure/InitialDbContent/InitialDbContent.cs
@@ -19,13 +19,13 @@ public class InitialDbContent : IInitialDbContent
     {
         var myType=typeof(T);
         var tProp = myType.GetProperties();
-        var w = tProp.FirstOrDefault (x => x.Name.Contains(myType.Name+"Id")) 
+        var idProp = tProp.FirstOrDefault (x => x.Name.Contains(myType.Name+"Id")) 
                 ?? throw new ArgumentNullException("tProp.FirstOrDefault (x => x.Name.Contains(myType.Name+\"Id\"))");
         string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Resources", "DbSeed",
             jsonFile);
         var resultData = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(filePath));
         int i = 0;
-        resultData!.ForEach(p=>w.SetValue(p, ++i) );
+        resultData!.ForEach(p=>idProp.SetValue(p, ++i) );
         return resultData;
     }
 }
